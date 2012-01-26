5c7aae07be8401c15eb8948848683b0d7cafad01
Ryan Northey <ryan@3ca.org.uk>
2012-01-26 10:48:16 +0000

    bit.core

--- bit/core/interfaces.py
+++ bit/core/interfaces.py
@@ -2,20 +2,24 @@
 
 from zope.interface import Interface as I
 
-
-class INamecoin(I):
-    def get_blockcount():
-        """ return the blockcount of the current block """
-
-    def get_name(name):
-        """ return value for name """
-
-    def get_dns(name,type='name'):
-        """ return dns record for given name """
-
-    def name_scan(first="",count=10):
-        """ return $count names starting from $first """
-
-
-class INamecoinDNS(I):
+class IServices(I):
     pass
+
+class IApplication(I):
+    pass
+
+class IPlugin(I):
+    pass
+
+class IPlugins(I):
+    pass
+
+class IConfiguration(I):
+    pass
+
+class IFileConfiguration(I):
+    pass
+
+class ISockets(I):
+    pass
+
